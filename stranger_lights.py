5be31dc43e5d55db5fe2f88dfcb726e5975951e4
JStav <hiorelse@gmail.com>
2017-10-01 14:20:16 -0400

    Test special characters

--- stranger_lights.py
+++ stranger_lights.py
@@ -153,6 +153,6 @@ if __name__ == '__main__':
 
 	print ('Press Ctrl-C to quit.')
 	while True:
-		show_word(strip, 'abcdefghijklmnopqrstuvwxyz')
+		show_word(strip, 'ABCDEFG HELLO ThisIs me!~')
 		random_color_wipe(strip)
 
