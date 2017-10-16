8b650eed067d7f7010b16fd4e373bc6b849c6aea
JStav <hiorelse@gmail.com>
2017-10-15 20:59:59 -0400

    Add normal mode

--- stranger_lights.py
+++ stranger_lights.py
@@ -76,17 +76,20 @@ class LightDriver:
 		self.strip.show()
 
 	def normal_mode(self):
-		self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+		self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, 
+			self.LED_BRIGHTNESS, self.LED_CHANNEL, self.LED_STRIP)
 		self.strip.begin()
 
 		self.all_on()
 
-		self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 155, LED_CHANNEL, LED_STRIP)
+		self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, 
+			155, self.LED_CHANNEL, self.LED_STRIP)
 		self.strip.begin()
 
 		self.all_on()
 
-		self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+		self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, 
+			self.LED_BRIGHTNESS, self.LED_CHANNEL, self.LED_STRIP)
 		self.strip.begin()
 
 		self.all_on()
