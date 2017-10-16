e35403ec37390721c08c281e801e54f581ae8ff6
JStav <hiorelse@gmail.com>
2017-10-15 21:01:55 -0400

    Add normal mode

--- stranger_lights.py
+++ stranger_lights.py
@@ -76,20 +76,32 @@ class LightDriver:
 		self.strip.show()
 
 	def normal_mode(self):
-		self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, 
-			self.LED_BRIGHTNESS, self.LED_CHANNEL, self.LED_STRIP)
+
+		# LED strip configuration:
+		LED_COUNT      = 50      # Number of LED pixels.
+		LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+		#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+		LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+		LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+		LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+		LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+		LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+		LED_STRIP      = ws.WS2811_STRIP_RGB   # Strip type and colour ordering
+
+		self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
+			LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 		self.strip.begin()
 
 		self.all_on()
 
-		self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, 
-			155, self.LED_CHANNEL, self.LED_STRIP)
+		self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
+			155, LED_CHANNEL, LED_STRIP)
 		self.strip.begin()
 
 		self.all_on()
 
-		self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, 
-			self.LED_BRIGHTNESS, self.LED_CHANNEL, self.LED_STRIP)
+		self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
+			LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 		self.strip.begin()
 
 		self.all_on()
