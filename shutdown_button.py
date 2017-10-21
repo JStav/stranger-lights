c3d3bc771ad023e4434efdb8903ad1d61ead3e1b
JStav <hiorelse@gmail.com>
2017-10-21 19:47:19 -0400

    Use #23

--- shutdown_button.py
+++ shutdown_button.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(18)
+    input_state = GPIO.input(23)
     if input_state == False:
         print('Button Pressed')
         time.sleep(0.2)
